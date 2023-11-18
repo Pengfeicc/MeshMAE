26f6eb13e29bc5dfc72553f11c0597fa0000cf44
chu <87825210+Pengfeicc@users.noreply.github.com>
2023-11-18 11:48:59 +0100

    Update show.py

--- show.py
+++ show.py
@@ -15,14 +15,14 @@ fname = "example"
 
 # filenames
 fpreds = Path("predictions/19-009.txt")
-fobj = Path("dataset/alien_small/test/alien/19-009.obj")
-fjson = Path("dataset/alien_small/test/alien/19-009.json")
+fobj = Path("dataset/xxx xxx/test/alien/19-009.obj")
+fjson = Path("dataset/xxx xxx/test/alien/19-009.json")
 
 # load files
 mesh = o3d.io.read_triangle_mesh(str(fobj)).compute_vertex_normals()
 preds = np.loadtxt(fpreds).astype(int) # 将fpreds转换成numpy格式, 输出[2,2,2 ... 0,0,0]共计16384个vertices
 
-"""
+
 with open(fjson, "r") as f: # “r”表示只读取文件内容
     data = json.load(f)
     raw_labels = data["raw_labels"] # 从 data 字典中提取名为 "raw_labels" 对应的值，并将其赋给 raw_labels
@@ -30,9 +30,8 @@ with open(fjson, "r") as f: # “r”表示只读取文件内容
 
     print(type(raw_labels), len(raw_labels))
     print(type(sub_labels), len(sub_labels))
-"""
+
 preds = preds.tolist()
-print(type(preds),len(preds))
 
 vertex_colors = np.zeros_like(mesh.vertices) # 初始化，其形状（shape）与 mesh.vertices 相同
 
@@ -42,48 +41,3 @@ for face_id in range(len(mesh.triangles)): # 遍历所有三角网格的数量
     vertex_colors[mesh.triangles[face_id]] = color
 mesh.vertex_colors = o3d.utility.Vector3dVector(vertex_colors)
 o3d.visualization.draw_geometries([mesh])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-# Initialize an empty list to store submeshes
-submeshes = []
-
-# Loop through unique labels
-unique_labels = np.unique(preds)
-for label in unique_labels:
-    # Create a submesh for the current label
-    submesh_indices = np.where(preds == label)[0]
-    submesh = mesh.select_by_index(submesh_indices)
-
-    # Assign a color to the submesh based on the label
-    # You can customize the colors as needed
-    if label == 0:
-        color = [1, 0, 0]  # Red
-    elif label == 1:
-        color = [0, 1, 0]  # Green
-    elif label == 2:
-        color = [0, 0, 1]  # Blue
-    elif label == 3:
-        color = [1, 1, 0]  # Yellow
-    else:
-        color = [1, 1, 1]  # Default to white for other labels
-    submesh.paint_uniform_color(color)
-    # Add the submesh to the list
-    submeshes.append(submesh)
-# Visualize all submeshes together
-o3d.visualization.draw_geometries(submeshes)
-"""
